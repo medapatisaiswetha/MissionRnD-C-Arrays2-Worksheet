338a7851a88bcd3a7d9c73d833288ff73db10a6b
medapatisaiswetha <medapatisaiswetha@missionrnd.com>
2016-03-30 22:07:29 +0530

    Submit Commit

--- src/countGreaterNumbers.cpp
+++ src/countGreaterNumbers.cpp
@@ -13,13 +13,92 @@ ERROR CASES: Return NULL for invalid inputs.
 
 NOTES:
 */
+#include <iostream>
+#include <malloc.h>
 
-struct transaction {
+struct transaction
+{
 	int amount;
 	char date[11];
 	char description[20];
 };
 
-int countGreaterNumbers(struct transaction *Arr, int len, char *date) {
-	return -1;
+int countGreaterNumbers(struct transaction *Arr, int len, char *date)
+{
+	if (Arr == 0 || len <= 0)
+		return NULL;
+	else
+	{
+		int i = 0, k = 0, d = 0, m = 0, y = 0, c = 0, res = 0, m1 = 0, d1 = 0, y1 = 0;
+		while (date[i] != '\0')
+		{
+			if (date[i] != '-')
+			{
+				if (date[i] >= 48 && date[i] <= 57)
+				{
+					if (c == 0)
+					{
+						d = (d * 10) + (date[i] - '0');
+					}
+					if (c == 1)
+					{
+						m = (m * 10) + (date[i] - '0');
+					}
+					if (c == 2)
+					{
+						y = (y * 10) + (date[i] - '0');
+					}
+				}
+			}
+			else if (date[i] == '-')
+			{
+				c++;
+			}
+			i++;
+		}
+		i = 0, c = 0;
+		while (k < len)
+		{
+			while (Arr[k].date[i] != '\0')
+			{
+				if (Arr[k].date[i] != '-')
+				{
+					if (Arr[k].date[i] >= 48 && Arr[k].date[i] <= 57)
+					{
+						if (c == 0)
+						{
+							d1 = (d1 * 10) + (Arr[k].date[i] - '0');
+						}
+						if (c == 1)
+						{
+							m1 = (m1 * 10) + (Arr[k].date[i] - '0');
+						}
+						if (c == 2)
+						{
+							y1 = (y1 * 10) + (Arr[k].date[i] - '0');
+						}
+					}
+				}
+				else if (Arr[k].date[i] == '-')
+				{
+					c++;
+				}
+				i++;
+			}
+			if (Arr[k].date[i] == '\0')
+			{
+				if (y1 >y)
+				{
+					res++;
+				}
+			}
+			k++;
+			i = 0;
+			c = 0;
+			d1 = 0;
+			m1 = 0;
+			y1 = 0;
+		}
+		return res;
+	}
 }
